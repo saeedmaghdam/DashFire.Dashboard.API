c643a23fd1c38369e8235b2e6bb6d08ff9025c70
saeedmaghdam <me.register@outlook.com>
2021-07-15 23:34:06 +0430

    Initialized

---
+++ src/DashFire.Dashboard.API/Apis/V1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashFire.Dashboard.API.Apis.V1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Done!");
+        }
+    }
+}
