f172c11cc18608b8f0bcea38c34c31b6fcb0b90d
Saeed Mohammadi <s.mohammadi@mabnadp.com>
2021-07-20 14:48:35 +0430

    #5 Update job's last execution status in database.

--- src/DashFire.Dashboard.Framework/Services/Job/IJobService.cs
+++ src/DashFire.Dashboard.Framework/Services/Job/IJobService.cs
@@ -6,5 +6,7 @@ namespace DashFire.Dashboard.Framework.Services.Job
     public interface IJobService
     {
         Task<long> UpsertAsync(string key, string instanceId, string parameters, CancellationToken cancellationToken);
+
+        Task PatchJobExecutionStatus(string key, string instanceId, CancellationToken cancellationToken);
     }
 }
