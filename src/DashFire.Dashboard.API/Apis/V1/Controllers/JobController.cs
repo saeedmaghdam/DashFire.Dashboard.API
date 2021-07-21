b3126aee07f3aaa721d70c1a1a534b1a93b71966
saeedmaghdam <me.register@outlook.com>
2021-07-21 17:25:54 +0430

    #13 Added job's GET service.

---
+++ src/DashFire.Dashboard.API/Apis/V1/Controllers/JobController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DashFire.Dashboard.Framework.Cache;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DashFire.Dashboard.API.Apis.V1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class JobController : Controller
+    {
+        private readonly DashFireCacheManager _cacheManager;
+
+        public JobController(DashFireCacheManager cacheManager)
+        {
+            _cacheManager = cacheManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(CancellationToken cancellationToken)
+        {
+            var jobs = await _cacheManager.GetJobsAsync(cancellationToken);
+            return Ok(jobs.Select(x=> new Models.Job.IndexViewModel()
+            {
+                Key = x.Key,
+                InstanceId = x.InstanceId
+            }));
+        }
+    }
+}
---
+++ src/DashFire.Dashboard.API/Apis/V1/Models/Job/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DashFire.Dashboard.API.Apis.V1.Models.Job
+{
+    public class IndexViewModel
+    {
+        public string Key
+        {
+            get;
+            set;
+        }
+
+        public string InstanceId
+        {
+            get;
+            set;
+        }
+    }
+}
